97cf44068c8ea8fc4c938f4dd9fba67302e18071
mansourelbash <mansour.programmer@gmail.com>
2025-02-11 10:23:45 +0300

    modify the config file for tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,11 @@ export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  purge: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  purge: ["./src/**/*.js", "./components/**/*.js"],
   safelist: [
     "max-w-[108rem]",
     "container",
